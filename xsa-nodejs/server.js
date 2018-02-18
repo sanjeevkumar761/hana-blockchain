b1f43a407630a196c7c95f04f8754ae3e3295498
sanjeevkumar761 <sanjeevkumar761@gmail.com>
2018-02-18 22:54:48 +0100

    Create server.js

---
+++ xsa-nodejs/server.js
@@ -0,0 +1,49 @@
+/*eslint no-console: 0, no-unused-vars: 0*/
+"use strict";
+
+var xsjs  = require("@sap/xsjs");
+var xsenv = require("@sap/xsenv");
+
+var port  = process.env.PORT || 3000;
+
+var options = {
+	anonymous : true, // remove to authenticate calls
+	redirectUrl : "/index.xsjs"
+};
+
+// configure HANA
+try {
+	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
+// configure UAA
+try {
+	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
+// start server
+xsjs(options).listen(port);
+
+console.log("Server listening on port %d", port);
+
+var request = require('request');
+
+	var options = {
+	  uri: 'http://148.100.99.170:3000/api/org.acme.sample.SetSensorTemp',
+	  method: 'POST',
+	  json: {
+	    			"$class": "org.acme.sample.SetSensorTemp",
+				  "gauge": "resource:org.acme.sample.Sensor#6146",
+				  "newSensorValue": 19
+	  }
+	};
+	
+	request(options, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body.id); // Print the shortened url.
+	  }
+});
